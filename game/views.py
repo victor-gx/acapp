0a2871e708843621404b99241be958b74aa0bd86
xing gao <2416457672@qq.com>
2022-04-24 12:59:39 +0800

    first html

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(resquest):
+    line1 = '<h1 style="text-align: center">我的第一个网页<h1>'
+    return HttpResponse(line1)
